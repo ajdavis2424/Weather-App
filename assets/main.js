8e7ee84cd38360d991823cd0db69bc65541a3ba6
ALexander Davis <ajdavis2424@gmail.com>
2021-08-02 19:29:34 -0400

    added weather API

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,6 @@
-window
+// create object for storing functions and variables for using weather api
+// need API key to access weather via onecall
+
+let weather = {
+    "apiKey": "1304e430a55b09b2296e48b2c51a5161"
+}
