e8168206b10ac9669d117549c0ee68f1fbd38ffc
ALexander Davis <ajdavis2424@gmail.com>
2021-08-02 21:39:50 -0400

    wore javascript

--- assets/main.js
+++ assets/main.js
@@ -2,5 +2,20 @@
 // need API key to access weather via onecall
 
 let weather = {
-    "apiKey": "1304e430a55b09b2296e48b2c51a5161"
-}
+    apiKey: "1304e430a55b09b2296e48b2c51a5161",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?q="
+        + city 
+        + "&appid="
+        + appKey
+        )
+
+        .then((response) => response.json())
+        .then((data) =>console.log(data));
+    },
+            // Display weather on page
+    displayWeather: function(data){
+        
+    }
+};
